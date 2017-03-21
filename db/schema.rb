d1dd3ec3e2f96dee7a0811cd739ff32df088db56
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-21 11:59:27 +0100

    add Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321103743) do
+ActiveRecord::Schema.define(version: 20170321105824) do
 
-  create_table "to_do_lists", force: :cascade do |t|
-    t.string   "task"
-    t.string   "task_details"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.string   "deatils"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

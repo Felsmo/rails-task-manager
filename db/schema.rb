f19b877cee1531937441a1eb2a5980891b687cea
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-21 12:02:53 +0100

    removed type

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170321105824) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.string   "deatils"
+    t.string   "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
